0d78de231b847982d0bfda80385ab22f81075744
Gabriel Torres Cecconi <seu-email-do-git@gmail.com>
2023-03-14 15:08:25 -0300

    Instance IaC changes

--- IaC/Instance/ec2.tf
+++ IaC/Instance/ec2.tf
@@ -24,8 +24,8 @@ resource "aws_instance" "instance" {
   count = 4
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3a.medium"
-  vpc_security_group_ids      = [""]
-  subnet_id = ""
+  vpc_security_group_ids      = ["sg-0fc985488a89b9abf"]
+  subnet_id = "subnet-0a7e02cd7fcbafeb0"
   associate_public_ip_address = true
 
   tags = {
@@ -35,6 +35,6 @@ resource "aws_instance" "instance" {
 
 resource "aws_eip" "eip" {
   count = 4
-  instance = aws_instance.instance[count.index]
+  instance = element(aws_instance.instance.*.id, count.index)
   vpc      = true
 }
